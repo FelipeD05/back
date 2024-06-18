59d9d648fdafec6bc20a16c94ba5f7991d411713
FelipeD05 <152222866+FelipeD05@users.noreply.github.com>
2024-06-17 21:22:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const getAllUsers = require("./routes/adminGetAllUsers");
+const login = require("./routes/login");
 
-const port = 5000
+let app = express();
+const port = 3000;
 
-const app = express();
-
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 
 app.get('/', (req, res) => { 
-    res.send('hola') 
+    res.send('miaus') 
     res.end() 
-}) 
+})
 
-app.listen(port,console.log(
-  'Server started on port' ));
+app.use("/admin/allUsers", getAllUsers)
+app.use("/logindata/", login);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    console.error(err.message, err.stack);
+    res.status(statusCode).json({ message: err.message });
+    return;
+});
+
+app.listen(port, () => {
+    console.log(Example app listening at http://localhost:${port});
+});
